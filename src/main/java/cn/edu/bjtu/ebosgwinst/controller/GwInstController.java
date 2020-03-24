a2aa7a401c3038d3803bd0388fcee4f169c7bc8f
Voo-C <voodoo.7@outlook.com>
2020-03-24 20:10:43 +0800

    backup & recover for command

--- src/main/java/cn/edu/bjtu/ebosgwinst/controller/GwInstController.java
+++ src/main/java/cn/edu/bjtu/ebosgwinst/controller/GwInstController.java
@@ -1,9 +1,31 @@
 package cn.edu.bjtu.ebosgwinst.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RequestMapping("/api/instance")
 @RestController
 public class GwInstController {
+    @Autowired
+    RestTemplate restTemplate;
+    private String commandUrl = "http://localhost:8082/api/command";
+
+    @CrossOrigin
+    @GetMapping()
+    public JSONObject getInfo(){
+        JSONObject result = new JSONObject();
+        JSONArray commandArray = new JSONArray(restTemplate.getForObject(commandUrl,JSONArray.class));
+        result.put("command",commandArray);
+        return result;
+    }
+
+    @CrossOrigin
+    @PostMapping()
+    public String putInfo(@RequestBody JSONObject info){
+        JSONArray commandArray = info.getJSONArray("command");
+        return restTemplate.postForObject(commandUrl+"/recover",commandArray,String.class);
+    }
 }
