a5ac1978c22709853a4dae9de09b8c36fc887ea2
Li-111 <358111907@qq.com>
2020-10-14 09:43:37 +0800

    Delete registration to cloud

--- src/main/java/cn/edu/bjtu/ebosgwinst/service/RegistrationService.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.bjtu.ebosgwinst.service;
-
-import cn.edu.bjtu.ebosgwinst.entity.Registration;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface RegistrationService {
-    String registration(Registration registration);
-}
